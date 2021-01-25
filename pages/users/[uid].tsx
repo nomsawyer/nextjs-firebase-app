c80edf1642b12bf7382b44f02ef772970e923563
tokio <tokio0813@gmail.com>
2021-01-25 22:14:33 +0900

    Added posting function

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { User } from '../../models/User'
 import { useRouter } from 'next/router'
 import firebase from 'firebase/app'
@@ -10,6 +10,9 @@ type Query = {
 
 export default function UserShow() {
   const [user, setUser] = useState<User>(null)
+  const [body, setBody] = useState('')
+  const [isSending, setIsSending] = useState(false)
+
   const router = useRouter()
   const query = router.query as Query
 
@@ -35,6 +38,21 @@ export default function UserShow() {
     loadUser()
   }, [query.uid])
 
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setIsSending(true)
+    await firebase.firestore().collection('questions').add({
+      senderUid: firebase.auth().currentUser.uid,
+      receiverUid: user.uid,
+      body,
+      isReplied: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+    setIsSending(false)
+    setBody('')
+    alert('質問を送信しました。')
+  }
+
   return (
     <Layout>
       {user && (
@@ -43,6 +61,31 @@ export default function UserShow() {
           <div className='m-5'>{user.name}さんに質問しよう！</div>
         </div>
       )}
+      <div className='row justify-content-center mb-3'>
+        <div className='col-12 col-md-6'>
+          <form onSubmit={onSubmit}>
+            <textarea
+              className='form-control'
+              placeholder='おげんきですか？'
+              rows={6}
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              required
+            ></textarea>{' '}
+            <div className='m-3'>
+              {isSending ? (
+                <div className='spinner-border text-secondary' role='status'>
+                  <span className='visually-hidden'>Loading...</span>
+                </div>
+              ) : (
+                <button type='submit' className='btn btn-primary'>
+                  質問を送信する
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
     </Layout>
   )
 }
